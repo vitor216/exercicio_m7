497c3658cc75bb35e91e0cb7f75085a8ec6072cf
vitor <aguiarvitor@gmail.com>
2022-11-13 22:30:50 -0300

    alterando o arquivo main.js

--- main.js
+++ main.js
@@ -15,12 +15,12 @@ form.addEventListener('submit', function(e){
     const mensagemSucesso = ('Os valores ' + primeiroNumero.value + ' e ' + segundoNumero.value + ' foram Salvos');
     const mensagemerror = ('O segundo numero tem que ser maior que o primeiro');
 
-    if(primeiroNumero.value < segundoNumero.value){
+    if(segundoNumero.value > primeiroNumero.value){
         alert(mensagemSucesso)
 
         primeiroNumero.value = '';
         segundoNumero.value = '';
-        
+
     }else{
         alert(mensagemerror)
     }})
