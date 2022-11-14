31bdcfc7c902c6b6783afb735873688906456639
vitor <aguiarvitor@gmail.com>
2022-11-13 22:05:04 -0300

    alterando o arquivo main.js

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ function removeelement(element){
     }
 
 form.addEventListener('submit', function(e){
+    var formEvalido = false;
     e.preventDefault();
 
     const primeiroNumero = document.getElementById('numero1');
     const segundoNumero = document.getElementById('numero2');
     const mensagemSucesso = ('Os valores ' + primeiroNumero.value + ' e ' + segundoNumero.value + ' foram Salvos');
     const mensagemerror = ('O segundo numero tem que ser maior que o primeiro');
-    
+    formEvalido = true;
     if(primeiroNumero.value < segundoNumero.value){
         const containermensagemsucesso = document.querySelector('.sucess-mensage');
         containermensagemsucesso.innerHTML = mensagemSucesso;
