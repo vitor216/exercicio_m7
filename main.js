f863823341b2be4490502f3525ef26c7cb637ba4
vitor <aguiarvitor@gmail.com>
2022-11-13 22:23:52 -0300

    alterando o arquivo main.js

--- main.js
+++ main.js
@@ -14,18 +14,13 @@ form.addEventListener('submit', function(e){
     const segundoNumero = document.getElementById('numero2');
     const mensagemSucesso = ('Os valores ' + primeiroNumero.value + ' e ' + segundoNumero.value + ' foram Salvos');
     const mensagemerror = ('O segundo numero tem que ser maior que o primeiro');
-    if (formEvalido){
+
     if(primeiroNumero.value < segundoNumero.value){
-        const containermensagemsucesso = document.querySelector('.sucess-mensage');
-        containermensagemsucesso.innerHTML = mensagemSucesso;
-        containermensagemsucesso.style.display='block'
-        removeelement(containermensagemsucesso);
-        
+        alert(mensagemSucesso)
+
         primeiroNumero.value = '';
         segundoNumero.value = '';
+        
     }else{
-        const containermensagemerror = document.querySelector('.error-mensage');
-        containermensagemerror.innerHTML = mensagemerror;
-        containermensagemerror.style.display='block'
-        removeelement(containermensagemerror);
-    }}})
+        alert(mensagemerror)
+    }})
